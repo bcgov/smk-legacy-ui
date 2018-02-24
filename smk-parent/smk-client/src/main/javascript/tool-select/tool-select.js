6cc159761e4bf7c1468bf11f4957e1b2ced23766
ben jubb <benjubb@gmail.com>
2018-02-24 14:12:25 -0800

    select tool

--- smk-parent/smk-client/src/main/javascript/tool-select/tool-select.js
+++ smk-parent/smk-client/src/main/javascript/tool-select/tool-select.js
@@ -1,79 +1,100 @@
-include.module( 'tool-select', [ 'smk', 'sidebar', 'tool-select.panel-select-html' ], function ( inc ) {
+include.module( 'tool-select', [ 'smk', 'tool', 'widgets', 'tool-select.panel-select-html' ], function ( inc ) {
 
-    return {
-        order: 5,
-        initialize: function ( smk, option ) {
-            var sb = smk.getSidebar()
+    Vue.component( 'select-widget', {
+        extends: inc.widgets.toolButton,
+    } )
 
-            var model = sb.addPanel( 'select', {
-                button: { title: 'Selection', icon: 'select_all' },
-                panel: {
-                    busy: false,
-                    layers: [],
-                    highlightId: null
-                }
-            } )
-
-            // sb.vm.$on( 'identifyPanel.active', function ( ev ) {
-            //     smk.$viewer.identified.pick( ev.feature.id )
-            // } )
-
-            // sb.vm.$on( 'identifyPanel.hover', function ( ev ) {
-            //     smk.$viewer.identified.highlight( ev.features && ev.features.map( function ( f ) { return f.id } ) )
-            // } )
-
-            // sb.vm.$on( 'identifyPanel.add-all', function ( ev ) {
-            // } )
-
-            // sb.vm.$on( 'identifyPanel.clear', function ( ev ) {
-            //     smk.$viewer.identified.clear()
-            // } )
-
-            // smk.$viewer.startedIdentify( function ( ev ) {
-            //     model.busy = true
-            // } )
-
-            // smk.$viewer.finishedIdentify( function ( ev ) {
-            //     model.busy = false
-            // } )
-
-            smk.$viewer.selected.addedFeatures( function ( ev ) {
-                sb.vm.$emit( 'activate-tool', { active: true, id: 'select' } )
-
-                var ly = smk.$viewer.layerId[ ev.layerId ]
-
-                model.layers[ ly.index ] = {
-                    id: ly.config.id,
-                    title: ly.config.title,
-                    features: ev.features
-                }
-            } )
-
-            // // smk.$viewer.selected.removedFeatures( function ( ev ) {
-            // // } )
-
-            // smk.$viewer.selected.pickedFeature( function ( ev ) {
-            //     model.highlightId = ev.feature && ev.feature.id
-            // } )
-
-            // // smk.$viewer.selected.highlightedFeatures( function ( ev ) {
-            // // } )
-
-            smk.$viewer.selected.clearedFeatures( function ( ev ) {
-                model.layers = []
-            } )
-
-            Vue.component( 'selectPanel', {
-                template: inc[ 'tool-select.panel-select-html' ],
-                props: [ 'layers', 'busy', 'highlightId' ],
-                methods: {
-                    isEmpty: function () {
-                        return !this.layers || this.layers.length == 0
-                    }
-                },
-            } )
-
-        }
+    Vue.component( 'select-panel', {
+        template: inc[ 'tool-select.panel-select-html' ],
+        props: [ 'tool' ],
+        methods: {
+            isEmpty: function () {
+                return !this.tool.layers || this.tool.layers.length == 0
+            }
+        },
+    } )
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    //
+    function SelectTool( option ) {
+        SMK.TYPE.Tool.prototype.constructor.call( this, $.extend( {
+            order:      5,
+            title:      'Selection',
+            busy:       false,
+            layers:     [],
+            highlightId: null,
+            widget: { icon: 'select_all', component: 'select-widget' },
+            panel: { component: 'select-panel' },
+            panelProperties: SMK.TYPE.Tool.prototype.panelProperties.concat( 'busy', 'layers', 'highlightId' )
+        }, option ) )
     }
 
+    SMK.TYPE.SelectTool = SelectTool
+
+    $.extend( SelectTool.prototype, SMK.TYPE.Tool.prototype )
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    //
+    SelectTool.prototype.afterInitialize = function ( smk, aux ) {
+        var self = this
+
+        aux.toolbar.vm.$on( 'select-widget.click', function () {
+            if ( !self.isVisible() || !self.isEnabled() ) return
+
+            self.active( !self.isActivated() )
+            // console.log( arguments )
+        } )
+
+        // sb.vm.$on( 'identifyPanel.active', function ( ev ) {
+        //     smk.$viewer.identified.pick( ev.feature.id )
+        // } )
+
+        // sb.vm.$on( 'identifyPanel.hover', function ( ev ) {
+        //     smk.$viewer.identified.highlight( ev.features && ev.features.map( function ( f ) { return f.id } ) )
+        // } )
+
+        // sb.vm.$on( 'identifyPanel.add-all', function ( ev ) {
+        // } )
+
+        // sb.vm.$on( 'identifyPanel.clear', function ( ev ) {
+        //     smk.$viewer.identified.clear()
+        // } )
+
+        // smk.$viewer.startedIdentify( function ( ev ) {
+        //     model.busy = true
+        // } )
+
+        // smk.$viewer.finishedIdentify( function ( ev ) {
+        //     model.busy = false
+        // } )
+
+        smk.$viewer.selected.addedFeatures( function ( ev ) {
+            self.active( true )
+            
+            // sb.vm.$emit( 'activate-tool', { active: true, id: 'select' } )
+
+            var ly = smk.$viewer.layerId[ ev.layerId ]
+
+            self.layers[ ly.index ] = {
+                id: ly.config.id,
+                title: ly.config.title,
+                features: ev.features
+            }
+        } )
+
+        // // smk.$viewer.selected.removedFeatures( function ( ev ) {
+        // // } )
+
+        // smk.$viewer.selected.pickedFeature( function ( ev ) {
+        //     self.highlightId = ev.feature && ev.feature.id
+        // } )
+
+        // // smk.$viewer.selected.highlightedFeatures( function ( ev ) {
+        // // } )
+
+        smk.$viewer.selected.clearedFeatures( function ( ev ) {
+            self.layers = []
+        } )
+
+    }
+
+    return SelectTool
 } )
