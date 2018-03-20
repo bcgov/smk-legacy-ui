d85f8bc656ab9e1e75d527aaeeb75c73a35a3da9
Benjamin Jubb <bjubb@vividsolutions.com>
2018-03-20 10:07:12 -0700

    fixes for identify

--- smk-parent/smk-client/src/main/javascript/layer.js
+++ smk-parent/smk-client/src/main/javascript/layer.js
@@ -142,7 +142,8 @@ include.module( 'layer', [ 'smk', 'jquery', 'util', 'event' ], function () {
             var serviceUrl  = this.config.serviceUrl
             var layerName   = this.config.layerName
             var styleName   = this.config.styleName
-            var version     = this.config.version || '1.1.1'
+            // var version     = this.config.version || '1.1.1'
+            var version     = '1.1.1'
 
             var params = {
                 service:       "WMS",
