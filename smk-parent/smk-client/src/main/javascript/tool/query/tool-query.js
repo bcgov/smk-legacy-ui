0038bdca12a5dca4d9239405ccf1c60351f35c88
ben jubb <benjubb@gmail.com>
2018-03-27 20:22:22 -0700

    fix style, add feature-set hooks

--- smk-parent/smk-client/src/main/javascript/tool/query/tool-query.js
+++ smk-parent/smk-client/src/main/javascript/tool/query/tool-query.js
@@ -58,10 +58,11 @@ include.module( 'tool-query', [ 'smk', 'tool', 'widgets', 'tool-query.panel-quer
 
         this.makePropPanel( 'busy', false )
         this.makePropPanel( 'queries', [] )
+        this.makePropPanel( 'selectedQueryId', null )
         this.makePropPanel( 'description', null )
         this.makePropPanel( 'parameters', null )
         this.makePropPanel( 'layerTitle', null )
-        this.makePropPanel( 'features', null )
+        this.makePropPanel( 'features', {} )
         this.makePropPanel( 'highlightId', null )
         this.makePropPanel( 'pickId', null )
 
@@ -81,10 +82,6 @@ include.module( 'tool-query', [ 'smk', 'tool', 'widgets', 'tool-query.panel-quer
     //
     QueryTool.prototype.afterInitialize.push( function ( smk, aux ) {
         var self = this
-
-        // smk.$viewer.handlePick( this, function ( location ) {
-        //     smk.$viewer.identifyFeatures( location )
-        // } )
 
         this.queries = Object.values( smk.$viewer.query ).map( function ( q ) {
             return {
@@ -111,14 +108,6 @@ include.module( 'tool-query', [ 'smk', 'tool', 'widgets', 'tool-query.panel-quer
             self.parameters[ ev.index ].prop.value = ev.value
         } )
 
-
-        // aux.panel.vm.$on( 'query-panel.hover', function ( ev ) {
-        //     smk.$viewer.identified.highlight( ev.features && ev.features.map( function ( f ) { return f.id } ) )
-        // } )
-
-        // aux.panel.vm.$on( 'query-panel.add-all', function ( ev ) {
-        // } )
-
         aux.panel.vm.$on( 'query-panel.clear', function ( ev ) {
             smk.$viewer.queried[ self.selectedQueryId ].clear()
 
@@ -142,49 +131,21 @@ include.module( 'tool-query', [ 'smk', 'tool', 'widgets', 'tool-query.panel-quer
                 } )
         } )
 
-        // smk.$viewer.pickedLocation( function () {
-        //     var enabledTools = Object.values( smk.$tool ).filter( function ( t ) { t.enabled } )
+        this.queries.forEach( function ( q ) {
+            var query = smk.$viewer.query[ q.id ]
 
+            smk.$viewer.queried[ q.id ].addedFeatures( function ( ev ) {
+                self.features[ q.id ] = ev.features
+            } )
+            
+            smk.$viewer.queried[ q.id ].pickedFeature( function ( ev ) {
+                self.pickId = ev.feature && ev.feature.id
+            } )
 
-        // } )
-
-        // smk.$viewer.startedIdentify( function ( ev ) {
-        //     self.busy = true
-        // } )
-
-        // smk.$viewer.finishedIdentify( function ( ev ) {
-        //     self.busy = false
-
-        //     if ( self.active && smk.$viewer.identified.isEmpty() )
-        //         self.active = false
-        // } )
-
-        // smk.$viewer.identified.addedFeatures( function ( ev ) {
-        //     self.active = true
-        //     // sb.vm.$emit( 'activate-tool', { active: true, id: 'identify' } )
-
-        //     var ly = smk.$viewer.layerId[ ev.layerId ]
-
-        //     self.layers[ ly.index ] = {
-        //         id: ly.config.id,
-        //         title: ly.config.title,
-        //         features: ev.features
-        //     }
-        // } )
-
-        // smk.$viewer.identified.removedFeatures( function ( ev ) {
-        // } )
-
-        // smk.$viewer.identified.pickedFeature( function ( ev ) {
-        //     self.highlightId = ev.feature && ev.feature.id
-        // } )
-
-        // smk.$viewer.identified.highlightedFeatures( function ( ev ) {
-        // } )
-
-        // smk.$viewer.identified.clearedFeatures( function ( ev ) {
-        //     self.layers = []
-        // } )
+            smk.$viewer.queried[ q.id ].clearedFeatures( function ( ev ) {
+                self.features[ q.id ] = null                
+            } )
+        } ) 
 
         // var el = smk.addToContainer( inc[ 'tool-query.popup-identify-html' ] )
 
