4067ca1ab47d987b43ec3511c8a454c8aad1d3c3
ben jubb <benjubb@gmail.com>
2018-04-17 20:27:49 -0700

    vector query

--- smk-parent/smk-client/src/main/javascript/query.js
+++ smk-parent/smk-client/src/main/javascript/query.js
@@ -160,13 +160,28 @@ include.module( 'query', [ 'smk', 'jquery', 'util', 'event' ], function () {
             var self = this
 
             var test = makeTest( this.predicate, param )
-            var res = []
+
+            var features = []
             viewer.visibleLayer[ layerId ].eachLayer( function ( ly ) {
                 if ( test( ly.feature.properties ) )
-                    res.push( ly.feature )
+                    features.push( ly.feature )
             } )
 
+            return SMK.UTIL.resolved( features )
+                .then( function ( features ) {
+                    console.log( features )
 
+                    if ( !features || features.length == 0 ) throw new Error( 'no features' )
+
+                    return features.map( function ( f, i ) {
+                        if ( self.layer.config.titleAttribute )
+                            f.title = f.properties[ self.layer.config.titleAttribute ]
+                        else
+                            f.title = 'Feature #' + ( i + 1 )
+
+                        return f
+                    } )
+                } )
             // var whereClause = makeWhereClause( this.predicate, param )
 
             // var attrs = this.layer.config.attributes.filter( function ( a ) { return a.visible !== false } ).map( function ( a ) { return a.name } )
@@ -223,9 +238,143 @@ include.module( 'query', [ 'smk', 'jquery', 'util', 'event' ], function () {
     } )
 
     function makeTest( predicate, param ) {
-
+        return makeTestOperator( predicate, param )
     }
 
+    function makeTestOperator( predicate, param ) {
+        if ( !( predicate.operator in testOperator ) )
+            throw new Error( 'unknown operator: ' + JSON.stringify( predicate ) )
+
+        return testOperator[ predicate.operator ]( predicate.arguments, param )        
+    }
+
+    var testOperator = {
+        'and': function ( args, param ) {
+            if ( args.length == 0 ) throw new Error( 'AND needs at least 1 argument' )
+
+            var tests = args.map( function ( a ) {
+                return makeTestOperator( a, param )
+            } )
+
+            return function ( properties ) {
+                return tests.every( function ( t ) {
+                    return t( properties )
+                } )
+            }
+        },
+
+        'or': function ( args, param ) {
+            if ( args.length == 0 ) throw new Error( 'AND needs at least 1 argument' )
+
+            var tests = args.map( function ( a ) {
+                return makeTestOperator( a, param )
+            } )
+
+            return function ( properties ) {
+                return tests.some( function ( t ) {
+                    return t( properties )
+                } )
+            }
+        },
+
+        'equals': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'EQUALS needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return a( properties ) == b( properties )
+            }
+        },
+
+        'less-than': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'LESS-THAN needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return a( properties ) < b( properties )
+            }
+        },
+
+        'greater-than': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'GREATER-THAN needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return a( properties ) > b( properties )
+            }
+        },
+
+        'contains': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'CONTAINS needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return ( new RegExp( b( properties ), 'i' ) ).test( a( properties ) )
+            }
+        },
+
+        'starts-with': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'STARTS-WITH needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return ( new RegExp( '^' + b( properties ), 'i' ) ).test( a( properties ) )
+            }
+        },
+
+        'ends-with': function ( args, param ) {
+            if ( args.length != 2 ) throw new Error( 'ENDS-WITH needs exactly 2 arguments' )
+
+            var a = makeTestOperand( args[ 0 ], param )
+            var b = makeTestOperand( args[ 1 ], param )
+
+            return function ( properties ) {
+                return ( new RegExp( b( properties ) + '$', 'i' ) ).test( a( properties ) )
+            }
+        },
+
+        'not': function ( args, param ) {
+            if ( args.length != 1 ) throw new Error( 'NOT needs exactly 1 argument' )
+
+            var a = makeTestOperator( args[ 0 ], param )
+
+            return function ( properties ) {
+                return ! a( properties )
+            }
+        }        
+    }
+
+    function makeTestOperand( predicate, param ) {
+        if ( !( predicate.operand in testOperand ) )
+            throw new Error( 'unknown operand: ' + JSON.stringify( predicate ) )
+
+        return testOperand[ predicate.operand ]( predicate, param )
+    }
+
+    var testOperand = {
+        'attribute': function ( arg, param, quote ) {
+            return function ( properties ) {
+                return properties[ arg.name ]
+            }
+        },
+
+        'parameter': function ( arg, param, quote ) {
+            return function ( properties ) {
+                return param[ arg.id ].value
+            }
+        }
+    }
+    
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     function makeWhereClause( predicate, param ) {
