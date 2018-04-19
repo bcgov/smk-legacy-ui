f5cbf2292f2d2edf33996f3f1368d68ca78839a6
Benjamin Jubb <bjubb@vividsolutions.com>
2018-04-19 16:48:28 -0700

    make zoom options true by default

--- smk-parent/smk-client/src/main/javascript/viewer-leaflet/tool/zoom/tool-zoom-leaflet.js
+++ smk-parent/smk-client/src/main/javascript/viewer-leaflet/tool/zoom/tool-zoom-leaflet.js
@@ -1,19 +1,19 @@
 include.module( 'tool-zoom-leaflet', [ 'tool-zoom', 'leaflet' ], function () {
 
     SMK.TYPE.ZoomTool.prototype.afterInitialize.push( function ( smk ) {
-        if ( this.mouseWheel ) {
+        if ( this.mouseWheel !== false ) {
             smk.$viewer.map.scrollWheelZoom.enable()
         }
 
-        if ( this.doubleClick ) {
+        if ( this.doubleClick !== false ) {
             smk.$viewer.map.doubleClickZoom.enable()
         }
 
-        if ( this.box ) {
+        if ( this.box !== false ) {
             smk.$viewer.map.boxZoom.enable()
         }
 
-        if ( this.control ) {
+        if ( this.control !== false ) {
             L.control.zoom().addTo( smk.$viewer.map )
         }
     } )
