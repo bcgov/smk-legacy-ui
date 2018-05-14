4d0bff82dfa30e239006bc7a88e78008eb90563e
ben jubb <benjubb@gmail.com>
2018-05-14 07:54:10 -0700

    fixed sort of

--- smk-parent/smk-client/src/main/javascript/viewer-esri3d/tool/location/tool-location-esri3d.js
+++ smk-parent/smk-client/src/main/javascript/viewer-esri3d/tool/location/tool-location-esri3d.js
@@ -2,29 +2,29 @@ include.module( 'tool-location-esri3d', [ 'esri3d', 'types-esri3d', 'tool-locati
 
     var E = SMK.TYPE.Esri3d
 
-    var base = include.option( 'baseUrl' ) + '/tool/directions'
+    // var base = include.option( 'baseUrl' ) + '/tool/directions'
 
-    var blueSymbol = {
-        type: 'point-3d',
-        symbolLayers: [
-            {
-                type:       'icon',
-                size:       '41px',
-                anchor:     'bottom',
-                resource: {
-                    href:   base + '/marker-shadow.png',
-                }
-            },
-            {
-                type:       'icon',
-                size:       '41px',
-                anchor:     'bottom',
-                resource: {
-                    href:   base + '/marker-icon-red.png',
-                }
-            }
-        ]
-    }
+    // var blueSymbol = {
+    //     type: 'point-3d',
+    //     symbolLayers: [
+    //         {
+    //             type:       'icon',
+    //             size:       '41px',
+    //             anchor:     'bottom',
+    //             resource: {
+    //                 href:   base + '/marker-shadow.png',
+    //             }
+    //         },
+    //         {
+    //             type:       'icon',
+    //             size:       '41px',
+    //             anchor:     'bottom',
+    //             resource: {
+    //                 href:   base + '/marker-icon-red.png',
+    //             }
+    //         }
+    //     ]
+    // }
 
     SMK.TYPE.LocationTool.prototype.afterInitialize.push( function ( smk ) {
         var self = this
@@ -41,7 +41,17 @@ include.module( 'tool-location-esri3d', [ 'esri3d', 'types-esri3d', 'tool-locati
         }
 
         this.updatePopup = function () {
-            smk.$viewer.view.popup.content = self.vm.$el
+            // this.location = location
+
+            // this.locationMarker.removeAll()
+
+            var g = { type: 'point', latitude: this.location.map.latitude, longitude: this.location.map.longitude }
+            smk.$viewer.view.popup.open( {
+                content: self.vm.$el,
+                location: g
+            } )
+
+            // smk.$viewer.view.popup.content = self.vm.$el
         }
 
         // this.popup = L.popup( {
@@ -50,26 +60,26 @@ include.module( 'tool-location-esri3d', [ 'esri3d', 'types-esri3d', 'tool-locati
         // } )
         // .setContent( function () { return self.vm.$el } )
 
-        this.locationMarker = new E.layers.GraphicsLayer()
-        smk.$viewer.map.add( this.locationMarker )
+        // this.locationMarker = new E.layers.GraphicsLayer()
+        // smk.$viewer.map.add( this.locationMarker )
 
         this.setLocation = function ( location ) {
             this.location = location
 
-            this.locationMarker.removeAll()
+            // this.locationMarker.removeAll()
 
             var g = { type: 'point', latitude: location.map.latitude, longitude: location.map.longitude }
-            this.locationMarker.add( new E.Graphic( {
-                geometry: g,
-                symbol: blueSymbol
-            } ) )
+            // this.locationMarker.add( new E.Graphic( {
+            //     geometry: g,
+            //     symbol: blueSymbol
+            // } ) )
 
             this.showPopup( g )
         }
 
         this.reset = function () {
             smk.$viewer.view.popup.close()
-            this.locationMarker.removeAll()
+            // this.locationMarker.removeAll()
             this.site = {}
         }
 
