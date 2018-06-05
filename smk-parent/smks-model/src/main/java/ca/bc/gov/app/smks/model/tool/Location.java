5e73215919d9746f02809ed9293a276fad982b44
Dylan <mondopotato_77@hotmail.com>
2018-06-05 16:10:33 -0700

    Fixed missing Json Include.NON_NULL directive for Location tool

--- smk-parent/smks-model/src/main/java/ca/bc/gov/app/smks/model/tool/Location.java
+++ smk-parent/smks-model/src/main/java/ca/bc/gov/app/smks/model/tool/Location.java
@@ -1,7 +1,11 @@
 package ca.bc.gov.app.smks.model.tool;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import ca.bc.gov.app.smks.model.Tool;
 
+@JsonInclude(Include.NON_NULL)
 public class Location extends Tool
 {
     public Location() {}
