a58a75c1e3e4c147d86b20de8277b2ce40325141
idir\dhemswor@bcgov <dhemsworth@vividsolutions.com>
2018-07-30 08:34:52 -0700

    Adding display property to layers tool

--- smk-parent/smks-model/src/main/java/ca/bc/gov/app/smks/model/tool/Layers.java
+++ smk-parent/smks-model/src/main/java/ca/bc/gov/app/smks/model/tool/Layers.java
@@ -1,7 +1,9 @@
 package ca.bc.gov.app.smks.model.tool;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import ca.bc.gov.app.smks.model.Tool;
 
@@ -10,6 +12,8 @@ public class Layers extends Tool
 {
     private static final long serialVersionUID = -4283508441394064225L;
 
+    private transient Object display;
+    
     public Layers() 
     {
         this.setTitle("Layers Panel");
@@ -21,6 +25,17 @@ public class Layers extends Tool
 		super( layers );
 	}
 	
+    @JsonRawValue
+    public JsonNode getDisplay() 
+    {
+        return (JsonNode)display;
+    }
+
+    public void setDisplay(JsonNode node) 
+    {
+        this.display = node;
+    }
+    
     @Override
     public boolean equals( Object other ) {
         return super.equals(other);
